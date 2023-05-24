165ca0d2d04469fe25e56c17cf24cfd70fb42462
Xinhao Yuan <llance_24@foxmail.com>
2023-05-24 22:20:57 +0800

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/LgoLgo
+
+go 1.18
---
+++ version.go
@@ -0,0 +1,7 @@
+package LgoLgo
+
+// Name and Version info of this framework, used for statistics and debug
+const (
+	Name    = "LgoLgo"
+	Version = "v0.0.1"
+)
